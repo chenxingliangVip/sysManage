021e2993bf85d913169e1447ce970556cb53e9be
zhanglu97 <1067351553@qq.com>
2020-09-15 10:14:09 +0800

    大屏修改

--- sysManage_vue/src/views/BigScreen/Statistics/Index.vue
+++ sysManage_vue/src/views/BigScreen/Statistics/Index.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="Statistics Statistics2" v-bind:style="style">
-		<nx-full-screen class="allfull screenfull" style="float: right"></nx-full-screen>
+		<nx-full-screen class="allfull screenfull" style="float: right; position: relative;    z-index: 111;"></nx-full-screen>
         <div class="header">
-            <p>威尔药业 · 生产信息统计表</p>
+            <p>威尔药业 · 智慧生产大数据平台</p>
+            <div class="TimeForm">
+                <p class="time">{{nowDate}} {{nowWeek}} {{nowTime}} </p>
+            </div>
         </div>
         <div class="main_contain">
             <el-row :gutter="6">
@@ -25,10 +28,10 @@
                             </tr>
                             <tr v-for="(item, index) in tableList" :key="index">
                                 <td width="10%">{{ index + 1 }}</td>
-                                <td width="25%"></td>
-                                <td width="25%"></td>
-                                <td width="25%"></td>
-                                <td width="15%"></td>
+                                <td width="25%">{{ item.tableData1 }}</td>
+                                <td width="25%">{{ item.tableData2 }}</td>
+                                <td width="25%">{{ item.tableData3 }}</td>
+                                <td width="15%">{{ item.tableData4 }}</td>
                             </tr>
                         </table>
                     </div>
@@ -42,28 +45,28 @@
                 <p class="tab_name"><i class="el-icon-d-arrow-right"></i>分析数据</p>
                 <el-row class="bottom_nav">
                     <el-col :span="6">
-                        <p class="nav">
-                            <span class="nav_left">检测样品数：</span>
-                            <span class="nav_right">20199</span>
-                        </p>
+                        <div class="nav">
+                            <p class="nav_right">20199</p>
+                            <p class="nav_left">检测样品数</p>
+                        </div>
                     </el-col>
                     <el-col :span="6">
-                        <p class="nav">
-                            <span class="nav_left">一次检样合格率：</span>
-                            <span class="nav_right">90%</span>
-                        </p>
+                        <div class="nav">
+                            <p class="nav_right">90%</p>
+                            <p class="nav_left">一次检样合格率</p>
+                        </div>
                     </el-col>
                     <el-col :span="6">
-                        <p class="nav">
-                            <span class="nav_left">差错率：</span>
-                            <span class="nav_right">1%</span>
-                        </p>
+                        <div class="nav">
+                            <p class="nav_right">0.5%</p>
+                            <p class="nav_left">差错率</p>
+                        </div>
                     </el-col>
                     <el-col :span="6">
-                        <p class="nav">
-                            <span class="nav_left">及时率：</span>
-                            <span class="nav_right">90%</span>
-                        </p>
+                        <div class="nav">
+                            <p class="nav_right">96%</p>
+                            <p class="nav_left">及时率</p>
+                        </div>
                     </el-col>
                 </el-row>
             </div>
@@ -79,8 +82,56 @@ export default {
     data() {
         return {
             style: {}, //缩放样式
+            nowTime: '',
+            nowDate: '',
+            nowWeek: '',
             tableList: [
-                {}
+                {
+                    tableData1: "2020-01-05",
+                    tableData2: "聚山梨酯80",
+                    tableData3: "20200101",
+                    tableData4: "6",
+                }, {
+                    tableData1: "2020-01-01",
+                    tableData2: "PEG 400",
+                    tableData3: "20200102",
+                    tableData4: "12",
+                }, {
+                    tableData1: "2020-01-01",
+                    tableData2: "PEG 4000",
+                    tableData3: "20200101",
+                    tableData4: "12",
+                }, {
+                    tableData1: "2019-12-25",
+                    tableData2: "聚山梨酯80",
+                    tableData3: "20200101",
+                    tableData4: "6",
+                }, {
+                    tableData1: "2019-12-20",
+                    tableData2: "PEG 400",
+                    tableData3: "20200102",
+                    tableData4: "12",
+                }, {
+                    tableData1: "2019-12-18",
+                    tableData2: "PEG 4000",
+                    tableData3: "20200101",
+                    tableData4: "12",
+                }, {
+                    tableData1: "2019-12-10",
+                    tableData2: "聚山梨酯80",
+                    tableData3: "20200102",
+                    tableData4: "6",
+                }, {
+                    tableData1: "2019-12-08",
+                    tableData2: "PEG 400",
+                    tableData3: "20200102",
+                    tableData4: "12",
+                }, {
+                    tableData1: "2019-12-07",
+                    tableData2: "PEG 4000",
+                    tableData3: "20200101",
+                    tableData4: "12",
+                },
             ]
         }
     },
@@ -103,12 +154,84 @@ export default {
             let echart3 = this.$echarts.init(this.$refs.echart3);
             echart3.setOption(echartOption.option4)
         },
+        getdateFormat() { //显示时间
+            var _this = this;
+            let wk = new Date().getDay()
+            let yy = String(new Date().getFullYear())
+            let mm = new Date().getMonth() + 1
+            let dd = String(new Date().getDate() < 10 ? '0' + new Date().getDate() : new Date().getDate())
+            let hou = String(new Date().getHours() < 10 ? '0' + new Date().getHours() : new Date().getHours())
+            let min = String(new Date().getMinutes() < 10 ? '0' + new Date().getMinutes() : new Date().getMinutes())
+            let sec = String(new Date().getSeconds() < 10 ? '0' + new Date().getSeconds() : new Date().getSeconds())
+            let weeks = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
+            let week = weeks[wk]
+
+            _this.nowTime = hou + ':' + min + ':' + sec;
+            _this.nowDate = yy + '/' + mm + '/' + dd
+            _this.nowWeek = week
+        },
+        barActive() {
+            let echart = this.$echarts.init(this.$refs.echart1);
+            let index = -1 //高亮所在下标
+            let index2 = -1 //高亮所在下标
+            let dataLength = echartOption.option1.series[0].data.length
+            let echart2 = this.$echarts.init(this.$refs.echart2);
+            let dataLength2 = echartOption.option2.series[0].data.length
+            setInterval(()=>{
+                // 清除之前的高亮
+                echart.dispatchAction({
+                    type: 'downplay',
+                    seriesIndex: 0,
+                    dataIndex: index
+                })
+                index = (index + 1) % dataLength
+                // 当前下标高亮
+                echart.dispatchAction({
+                    type: 'highlight',
+                    seriesIndex: 0,
+                    dataIndex: index
+                })
+				echart.dispatchAction({
+					type: 'showTip',
+					seriesIndex: 0,
+					dataIndex: index
+				});
+                // 清除之前的高亮
+                echart2.dispatchAction({
+                    type: 'downplay',
+                    seriesIndex: 0,
+                    dataIndex: index2
+                })
+                index2 = (index2 + 1) % dataLength2
+                // 当前下标高亮
+                echart2.dispatchAction({
+                    type: 'highlight',
+                    seriesIndex: 0,
+                    dataIndex: index2
+                })
+				echart2.dispatchAction({
+					type: 'showTip',
+					seriesIndex: 0,
+					dataIndex: index2
+				});
+                if (index > dataLength) {
+                    index = 0
+                }
+                if (index2 > dataLength2) {
+                    index2 = 0
+                }
+            }, 2000)
+        }
     },
     mounted() {
         // this.pageShow()
         this.getEchartData1()
         this.getEchartData2()
         this.getEchartData3()
+        this.barActive()
+		setInterval(() => {
+            this.getdateFormat()
+        }, 1000);
     },
     components: {
         nxFullScreen
--- sysManage_vue/src/views/BigScreen/Statistics/echart.js
+++ sysManage_vue/src/views/BigScreen/Statistics/echart.js
@@ -1,24 +1,24 @@
 var data1 = [87.5, 95.9, 79, 60]; //echart1 产品状态数据
-var data2 = [40, 90, 10, 20];//echart2 各生产部准点率
+var data2 = [88, 86, 91, 89];//echart2 各生产部准点率
 var data3 = [//echart3 各生产部准点率 酯化部
-    { "value": 3 },  
-    { "value": 6 },
-    { "value": 6 }
+    { "value": 880 },  
+    { "value": 670 },
+    { "value": 880 }
 ];
 var data4 = [//聚合部
-    { "value": 5 },  
-    { "value": 4 },
-    { "value": 2 }
+    { "value": 670 },  
+    { "value": 670 },
+    { "value": 670 }
 ];
 var data5 = [//封端部
-    { "value": 10 },  
-    { "value": 5 },
-    { "value": 7 }
+    { "value": 910 },  
+    { "value": 910 },
+    { "value": 910 }
 ];
 var data6 = [//药铺部
-    { "value": 4 },  
-    { "value": 4 },
-    { "value": 8 }
+    { "value": 550 },  
+    { "value": 550 },
+    { "value": 550 }
 ];
 
 export default {
@@ -27,14 +27,15 @@ export default {
             trigger: 'axis',
             padding: [10, 10],
             axisPointer: {
-                type: 'shadow'
+                type: 'line',
             },
+            backgroundColor: 'rgba(64, 200, 255, .4)',
             formatter: function (param) {
                 return param[0].name + "：" + param[0].value
             }
         },
         grid: {
-            left: 100,
+            left: 120,
             right: 48,
             top: 0,
             bottom: 0
@@ -56,10 +57,10 @@ export default {
         },
         yAxis: [{
             type: 'category',
-            data: ['PEG400\n（20200101）', 'PEG400\n（20200103）', '聚山梨酯20\n（20200105）', '聚山梨酯20\n（20200106）',],
+            data: ['PEG400\n20200101', 'PEG400\n20200103', '聚山梨酯20\n20200105', '聚山梨酯20\n20200106',],
             axisLabel: {
                 color: "#eeeeee",
-                fontSize: 12,
+                fontSize: 14,
                 lineHeight: 18,
             },
             axisTick: {
@@ -78,7 +79,7 @@ export default {
             name: '',
             type: 'bar',
             data: data1,
-            barWidth: 20,
+            barWidth: 26,
             zlevel: 1,
             itemStyle: {
                 normal: {
@@ -88,41 +89,44 @@ export default {
                         x2: 0,
                         y2: 0,
                         colorStops: [
-                            { offset: 0, color: 'rgba(135,183, 232, 1)' },
-                            { offset: 1, color: 'rgba(135, 183, 232, 0)' }
+                            { offset: 0, color: 'rgb(57,89,255,1)' },
+                            { offset: 1, color: 'rgb(46,200,207,1)' }
                         ]
                     },
-                    borderColor: {
-                        x: 1,
-                        y: 0,
-                        x2: 0,
-                        y2: 0,
-                        colorStops: [
-                            { offset: 0, color: 'rgba(180,240,255,1)' },
-                            { offset: 1, color: 'rgba(15,197,243,1)' }
-                        ]
-                    },
+                    barBorderRadius: 30,
+                    borderColor: 'rgba(180,240,255,.3)',
                     borderWidth: 1
                 }
             },
             label: {
                 normal: {
                     show: true,
-                    position: 'inside',
-                    formatter: '{c}'
-                }
+                    position: 'insideRight',
+                    formatter: '{c}',
+                    color: "#eeeeee",
+                    textStyle: {
+                        fontSize: 16,
+                        align: 'right'
+                    },
+                },
             },
+            emphasis: {
+                itemStyle: {
+                    borderColor: 'rgba(180,240,255,1)',
+                }
+            }
         },
         {
             name: '',
             type: 'bar',
-            barWidth: 20,
+            barWidth: 26,
             barGap: '-100%',
             data: [100, 100, 100, 100],
             itemStyle: {
                 normal: {
-                    color: '#1c2156'
-                }
+                    color: '#1c2156',
+                    barBorderRadius: 30,
+                },
             }
         }
         ]
@@ -130,13 +134,18 @@ export default {
 
     option2: {
         tooltip: {
-            formatter: '{b}：\n{c}%'
+            // trigger: 'axis',
+            formatter: '{b}：\n{c}%',
+            backgroundColor: 'rgba(64, 200, 255, .4)',
+            axisPointer: {
+                type: 'line',
+            },
         },
         grid: {
             left: 10,
             right: 10,
-            top: 10,
-            bottom: 30
+            top: 20,
+            bottom: 42
         },
         xAxis: [{
             type: 'category',
@@ -144,12 +153,12 @@ export default {
             axisLabel: {
                 color: '#eeeeee',
                 textStyle: {
-                    fontSize: 12
+                    fontSize: 16
                 },
             },
             axisLine: {
                 lineStyle: {
-                    color: '#d2d2d2',
+                    color: '#2ec8cf',
                 }
             },
             axisTick: {
@@ -175,7 +184,7 @@ export default {
         series: [{
             type: 'bar',
             data: data2,
-            barWidth: '30px',
+            barWidth: '40px',
             itemStyle: {
                 normal: {
                     color: {
@@ -193,94 +202,14 @@ export default {
             },
             label: {
                 show: true,
-                fontSize: 12,
+                fontSize: 16,
                 position: 'top',
-                color: '#eeeeee',
+                color: '#fbde0d',
                 formatter: '{c}%'
             }
         }]
     },
-    option3: {
-        tooltip: {
-            trigger: 'axis',
-            axisPointer: {
-                type: 'shadow'
-            }
-        },
-        color: ['#338cf9', '#96edc1', '#ea6b7c', '#fcb75b'],
-        grid: {
-            top: "30",
-            left: "10",
-            bottom: "30",
-            right: "10",
-        },
-        legend: {
-            show: true,
-            top: 0,
-            right: 26,
-            data: ['酯化部', '聚合部', '封端部', '药铺部'],
-            textStyle: {
-                color: "RGB(193,223,255)"
-            }
-        },
-        xAxis: [{
-            type: "category",
-            data: ["2020-01", "2020-02", "2020-03"],
-            axisTick: {
-                alignWithLabel: true
-            },
-            nameTextStyle: {
-                color: "#fff"
-            },
-            axisLine: {
-                lineStyle: {
-                    color: "RGB(47,68,114)"
-                }
-            },
-            axisLabel: {
-                textStyle: {
-                    color: "white"
-                },
-            }
-        }],
-        yAxis: [{
-            type: "value",
-            axisLabel: {
-                show: false,
-            },
-            splitLine: {
-                show: false,
-            },
-            axisLine: {
-                show: false,
-            }
-        }],
-        series: [{
-            name: '酯化部',
-            type: 'bar',
-            barWidth: "25",
-            data: data3
-        },
-        {
-            name: '聚合部',
-            type: 'bar',
-            barWidth: "25",
-            data: data4
-        },
-        {
-            name: '封端部',
-            type: 'bar',
-            barWidth: "25",
-            data: data5
-        },
-        {
-            name: '药铺部',
-            type: 'bar',
-            barWidth: "25",
-            data: data6
-        }
-        ]
-    },
+
     option4: {
         tooltip: {
             trigger: 'axis',
@@ -290,56 +219,66 @@ export default {
         },
         color: ['rgba(51, 140, 249, .7)', 'rgba(150, 237, 193,.7)', 'rgba(234, 107, 124, .7)', 'rgba(252, 183, 91,.7)'],
         grid: {
-            top: "30",
-            left: "10",
+            top: "40",
+            left: "200",
             bottom: "30",
-            right: "10",
+            right: "20",
         },
         legend: {
             show: true,
-            top: 0,
-            right: 26,
+            top: 80,
+            left: 30,
             data: ['酯化部', '聚合部', '封端部', '药铺部'],
             textStyle: {
-                color: "RGB(193,223,255)"
-            }
+                color: "#eeeeee",
+                fontSize: 16,
+                padding: [0, 0, 0, 15]
+            },
+            orient: 'vertical',
+            itemHeight: 20,
+            itemWidth: 40,
+            itemGap: 25
         },
         xAxis: [{
             type: "category",
             data: ["2020-01", "2020-02", "2020-03"],
             axisTick: {
-                alignWithLabel: true
+                show: false
             },
             nameTextStyle: {
                 color: "#fff"
             },
             axisLine: {
                 lineStyle: {
-                    color: "RGB(47,68,114)"
+                    color: "#2ec8cf"
                 }
             },
             axisLabel: {
                 textStyle: {
-                    color: "white"
+                    color: "#eeeeee",
+                    fontSize: 16,
                 },
             }
         }],
         yAxis: [{
             type: "value",
             axisLabel: {
-                show: false,
-            },
-            splitLine: {
-                show: false,
+                show: false
             },
             axisLine: {
-                show: false,
+                show: false
+            },
+            axisTick: {
+                show: false
+            },
+            splitLine: {
+                show: false
             }
         }],
         series: [{
             name: '酯化部',
             type: 'bar',
-            barWidth: "25",
+            barWidth: "35",
             data: data3,
             itemStyle: {
                 normal: {
@@ -349,17 +288,26 @@ export default {
                         x2: 0,
                         y2: 0,
                         colorStops: [
-                            { offset: 0, color: 'rgba(51, 140, 249, .2)' },
-                            { offset: 1, color: 'rgba(51, 140, 249, .9)' }
+                            { offset: 0,  color: "#f5c379" },
+                            { offset: 1,  color: "#f5c379" }
                         ]
                     },
-                }
+                    shadowBlur: 5,
+                    shadowColor: 'rgba(255, 255, 255, 0.3)',
+                },
+            },
+            label: {
+                show: true,
+                fontSize: 16,
+                position: 'top',
+                color: '#f5c379',
+                formatter: '{c}'
             },
         },
         {
             name: '聚合部',
             type: 'bar',
-            barWidth: "25",
+            barWidth: "35",
             data: data4,
             itemStyle: {
                 normal: {
@@ -369,17 +317,26 @@ export default {
                         x2: 0,
                         y2: 0,
                         colorStops: [
-                            { offset: 0, color: 'rgba(150, 237, 193, .2)' },
-                            { offset: 1, color: 'rgba(150, 237, 193, .9)' }
+                            { offset: 0, color: '#118df0' },
+                            { offset: 1, color: '#118df0' }
                         ]
                     },
+                    shadowBlur: 5,
+                    shadowColor: 'rgba(255, 255, 255, 0.3)',
                 }
             },
+            label: {
+                show: true,
+                fontSize: 16,
+                position: 'top',
+                color: '#118df0',
+                formatter: '{c}'
+            }
         },
         {
             name: '封端部',
             type: 'bar',
-            barWidth: "25",
+            barWidth: "35",
             data: data5,
             itemStyle: {
                 normal: {
@@ -389,17 +346,26 @@ export default {
                         x2: 0,
                         y2: 0,
                         colorStops: [
-                            { offset: 0, color: 'rgba(234, 107, 124, .2)' },
-                            { offset: 1, color: 'rgba(234, 107, 124, .9)' }
+                            { offset: 0, color: '#AF89D6' },
+                            { offset: 1, color: '#AF89D6' }
                         ]
                     },
+                    shadowBlur: 5,
+                    shadowColor: 'rgba(255, 255, 255, 0.3)',
                 }
             },
+            label: {
+                show: true,
+                fontSize: 16,
+                position: 'top',
+                color: '#AF89D6',
+                formatter: '{c}'
+            }
         },
         {
             name: '药铺部',
             type: 'bar',
-            barWidth: "25",
+            barWidth: "35",
             data: data6,
             itemStyle: {
                 normal: {
@@ -409,12 +375,21 @@ export default {
                         x2: 0,
                         y2: 0,
                         colorStops: [
-                            { offset: 0, color: 'rgba(252, 183, 91, .2)' },
-                            { offset: 1, color: 'rgba(252, 183, 91, .9)' }
+                            { offset: 0, color: "#11cbd7" },
+                            { offset: 1, color: '#11cbd7' }
                         ]
                     },
+                    shadowBlur: 5,
+                    shadowColor: 'rgba(255, 255, 255, 0.3)',
                 }
             },
+            label: {
+                show: true,
+                fontSize: 16,
+                position: 'top',
+                color: '#11cbd7',
+                formatter: '{c}'
+            }
         }
         ]
     },
